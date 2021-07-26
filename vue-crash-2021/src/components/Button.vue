17adc12b69bf950032b1b3e42db9f442c92fb9fe
matunascl <matunascl@gmail.com>
2021-07-26 11:50:58 -0400

    add tasks, header, button

---
+++ vue-crash-2021/src/components/Button.vue
@@ -0,0 +1,22 @@
+<template>
+  <button @click="onClick()" v-bind:style="{background: color}" class="btn">{{text}}</button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        text: String,
+        color: String
+    },
+    methods: {
+        onClick() {
+            console.log('click');
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-crash-2021/src/components/Tasks.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <section v-for="task in tasks" v-bind:key="task.id">
+        <Task v-bind:task="task"></Task>
+    </section>
+  </div>
+</template>
+
+<script>
+import Task from "./Task"
+
+export default {
+  name: 'Tasks',
+  props: {
+      tasks: Array
+  },
+  components: {
+      Task
+  }
+}
+</script>
+  
+<style>
+
+</style>
